bf727adc3c8fade1c10dfbaf5e620734f652923b
Pravin Varpe <pravinvarpe14@gmail.com>
2022-07-31 21:43:34 +0530

    Added perfect number programme

---
+++ src/Perfect_number.java
@@ -0,0 +1,33 @@
+/*
+ * perfect number
+ */
+import java.util.Scanner;
+
+public class Perfect_number {
+
+	public static void main(String[] args) {
+		int sum = 0, i;
+		/*
+		 * Taking input number from user
+		 */
+		System.out.println("Enter the number to be checked.. ");
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		/*
+		 * using for loop
+		 */
+		for (i = 1; i < n; i++) {
+
+			if (n % i == 0) {
+				sum = sum + i;
+			}
+
+		}
+		if (sum == n) {
+			System.out.println("The given number is a perfect number");
+		} else {
+			System.out.println("The given number is not a perfect number");
+		}
+	}
+	}
